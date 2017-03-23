77f275055cfebf8087caab5edc30edbffe80f855
ab14abw <ab14abw@herts.ac.uk>
2017-03-23 00:14:35 +0000

    Add files via upload

---
+++ pre school app for kids 4/pre school app for kidsTests/pre_school_app_for_kidsTests.swift
@@ -0,0 +1,36 @@
+//
+//  pre_school_app_for_kidsTests.swift
+//  pre school app for kidsTests
+//
+//  Created by ab14abw on 19/03/2017.
+//  Copyright © 2017 ab14abw. All rights reserved.
+//
+
+import XCTest
+@testable import pre_school_app_for_kids
+
+class pre_school_app_for_kidsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
